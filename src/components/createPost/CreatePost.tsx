e8a45232c81d6a86a668b4cd870478a52d11bf40
Kumar Piyush <kr.piyush888@gmail.com>
2024-09-09 00:17:47 +0530

    layout changes, added logo and post creation box

--- src/components/createPost/CreatePost.tsx
+++ src/components/createPost/CreatePost.tsx
@@ -1,23 +1,36 @@
 import { Input } from "@/components/ui/input";
 import { Card, CardHeader } from "../ui/card";
 import { Button } from "../ui/button";
-import Tiptap from "../tiptap/Tiptap";
 import { useState } from "react";
-// import { useCurrentEditor } from "@tiptap/react";
+import { Textarea } from "@/components/ui/textarea"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const CreatePost = () => {
     const [content, setContent] = useState<string>('Sample Content')
+    const [title, setTitle] = useState<string>('Sample Title')
     // const { editor } = useCurrentEditor()
+
+    async function postHandler() {
+        
+    }
   return (
-        <Card className="lg:w-full">
-            <CardHeader className="flex flex-col items-center gap-4">
-              <Input type={`text`} placeholder="Title" />
-              <Tiptap/>
-              <Button onClick = {() => {
-                console.log(content)
-              }}>Post</Button>
-            </CardHeader>
-        </Card>
+    <Card>
+    <CardHeader className="flex flex-row items-center gap-4 justify-between">
+      <Avatar>
+        <AvatarImage src="/placeholder-user.jpg" alt="@username" />
+        <AvatarFallback>UN</AvatarFallback>
+      </Avatar>
+      <div className = "flex flex-col w-full gap-6">
+        <Input placeholder="Post Title" onChange={(event) => {
+            setTitle(event.target.value)
+        }}/>
+        <Textarea placeholder="Post Content" onChange = {(event) => {
+            setContent(event.target.value);
+        }}/>
+        <Button className = "place-self-end w-1/6">Post</Button>
+      </div>
+    </CardHeader>
+  </Card>
   )
 }
 
